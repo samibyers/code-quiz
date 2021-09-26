5620ce388524cec6d6fef6648647291492681969
Sami Byers <samtaylor.byers@gmail.com>
2021-09-26 16:33:26 -0500

    all the files, html skeleton, start of JS

---
+++ script.js
@@ -0,0 +1,48 @@
+//Coding Quiz Challenge 
+//Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your scoretime by ten seconds!
+//button (Start Quiz)
+
+//Commonly used data types DO NOT include:
+//choices: strings, booleans, alerts(T), numbers
+//Correct! with upper border or Wrong!
+var strings = false
+var booleans = false
+var alerts = true
+var numbers = false
+if (alerts) {
+    console.log("Correct!")
+} else () {
+    console.log ("Wrong!")
+}
+
+//The condition in an if/ else statement is enclosed within ____.
+//choices: quotes, curly brackets(T), parentheses, square brackets 
+//Correct! with upper border or Wrong!
+var quotes = false
+var curlyBrackets = true
+var parentheses = false
+var squareBrackets = false
+if (curlyBrackets) {
+    console.log("Correct!")
+} else () {
+    console.log("Wrong!")
+}
+
+//Arrays in JavaScript can be used to store ____.
+//choices: numbers and strings, other arrays, booleans, all of the above(T)
+//Correct! with upper border or Wrong!
+
+//All done!
+//Your final score is (score)
+//enter initials (form), button: Submit
+
+//Highscores 
+//scores are displayed and saved
+// button: Go Back, button: clear highscores
+
+
+
+
+
+
+
